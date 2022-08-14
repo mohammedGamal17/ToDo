12dadaec1c64c3d7aa68af9337d87933210beb7c
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-08-14 12:16:07 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,32 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'layouts/home_screen/home_page.dart';
 import 'shared/bloc_observer.dart';
 
 void main() {
   BlocOverrides.runZoned(
-        () {
+    () {
+      WidgetsFlutterBinding.ensureInitialized();
+      SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown],
+      );
       runApp(const MyApp());
     },
     blocObserver: MyBlocObserver(),
   );
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:HomePage(),
-      color:const Color.fromRGBO(78, 148, 79, 1.0),
-
+      home: HomePage(),
+      color: const Color.fromRGBO(78, 148, 79, 1.0),
     );
   }
-
 }
